88408427649f82bdacea13bf5b03726f818c41fc
prokarius <rollingwithoutslipping@yahoo.com>
2019-03-03 00:07:42 -0800

    Make floaty

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var price = document.getElementById("prcIsum").getAttribute("content");
-var numPotatoes = parseInt(0.99+(parseFloat(price)/1.00));
+var numPotatoes = parseInt(0.9999+(parseFloat(price)/0.277));
 console.log(price);
 
-var node = document.getElementById("vi-mskumap-none");
+var node = document.getElementById("prcIsum");
 
 var newItem = document.createElement("div");       // Create a <li> node
 var textnode = document.createTextNode(" " + numPotatoes + " Potatoes");  // Create a text node
-newItem.appendChild(textnode);                    // Append the text to <li>
+
 newItem.setAttribute("id", "pop1");
 newItem.style.borderColor = "#444141";
 newItem.style.borderStyle = "solid";
@@ -14,23 +14,9 @@ newItem.style.backgroundColor = "#eaeaea";
 newItem.style.borderWidth = "3px";
 newItem.style.zIndex = "2";
 newItem.style.borderRadius = "10px";
-newItem.style.textAlign = "center"
+
+newItem.style.textAlign = "center";
 newItem.style.padding = "4px";
-newItem.style.display = "inline";
-newItem.style.position = "absolute";
-newItem.style.left = "120px";
-newItem.style.top = "245px";
 
+newItem.appendChild(textnode);                    // Append the text to <li>
 node.appendChild(newItem);
-
-
-/*
-	border-color: #444141;
-	border-style: solid;
-	border-width: 3px;
-	background-color: #eaeaea;
-    z-index: 2;
-    border-radius: 10px;
-
-
-*/
