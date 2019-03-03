7ae5111722731ef1e0a16b64f74a1deaa9b024fd
prokarius <rollingwithoutslipping@yahoo.com>
2019-03-02 22:38:45 -0800

    Add div popover

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ console.log(price);
 
 var node = document.getElementById("vi-mskumap-none");
 
-var newItem = document.createElement("SPAN");       // Create a <li> node
+var newItem = document.createElement("div");       // Create a <li> node
 var textnode = document.createTextNode(" " + numPotatoes + " Potatoes");  // Create a text node
 newItem.appendChild(textnode);                    // Append the text to <li>
+newItem.setAttribute("id", "pop1");
+newItem.style.borderColor = "#444141";
+newItem.style.borderStyle = "solid";
+newItem.style.backgroundColor = "#eaeaea";
+newItem.style.borderWidth = "3px";
+newItem.style.zIndex = "2";
+newItem.style.borderRadius = "10px";
+newItem.style.float = "left";
+newItem.style.textAlign = "center"
+newItem.style.padding = "4px";
+newItem.style.display = "inline";
+
 node.appendChild(newItem);
+
+
+/*
+	border-color: #444141;
+	border-style: solid;
+	border-width: 3px;
+	background-color: #eaeaea;
+    z-index: 2;
+    border-radius: 10px;
+
+
+*/
